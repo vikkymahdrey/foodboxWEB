86675cb86f69f96ccd395befbe6120c3b52af5ee
mightygithub <vikky@unizentechnologies.com>
2016-12-28 16:18:30 +0530

    Initial commit
    Initial commit

---
+++ foodyboxWEB/src/main/java/com/team/foodybox/dao/AdminDAO.java
@@ -0,0 +1,18 @@
+package com.team.foodybox.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.team.foodybox.domain.Viewer;
+
+public interface AdminDAO extends JpaRepository<Viewer, Integer> {
+
+	@Query("FROM Viewer")
+	List<Viewer> getViewer() throws Exception;
+
+	@Query("select count(v.id) FROM Viewer v")
+	Object getVisitorCount();
+
+}
---
+++ foodyboxWEB/src/main/java/com/team/foodybox/services/AdminService.java
@@ -0,0 +1,17 @@
+package com.team.foodybox.services;
+
+import java.util.List;
+
+import com.team.foodybox.domain.Viewer;
+
+public interface AdminService {
+
+	Viewer save(Viewer viewer) throws Exception;
+
+	String getMessageTemplate(Viewer viewers) throws Exception;
+
+	List<Viewer> getViewer()throws Exception;
+
+	String getVisitorCount()throws Exception;
+
+}
